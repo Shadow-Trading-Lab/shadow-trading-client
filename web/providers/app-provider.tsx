3f8920abe077c43934f37b4e9e4e2ce99eed0086
Elmer <s0227373691@gms.tku.edu.tw>
2024-03-27 20:10:47 +0800

    fixed

--- web/providers/app-provider.tsx
+++ web/providers/app-provider.tsx
@@ -7,9 +7,9 @@ type AppContextType = null
 
 export const AppContext = createContext<AppContextType>(null)
 
-export const AppProvider = ({ children }) => {
+export const AppProvider = ({ children }: {children: any}) => {
     return (
-       <AppContext.Provider value="">
+       <AppContext.Provider value={null}>
             <AlertProvider>
                 {children}
             </AlertProvider>
