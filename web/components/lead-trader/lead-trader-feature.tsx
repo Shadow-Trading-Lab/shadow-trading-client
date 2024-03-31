397dcc75c57f8df5769b358d4a21e15c66aa3cbf
Elmer <s0227373691@gms.tku.edu.tw>
2024-03-31 16:22:13 +0800

    hide alert

--- web/components/lead-trader/lead-trader-feature.tsx
+++ web/components/lead-trader/lead-trader-feature.tsx
@@ -1,12 +1,11 @@
 'use client';
 
 import React from 'react'
-import { PageTitle, LeaderCardList, AlertSuccess, SearchInput} from '@/components/lead-trader/lead-trader-ui'
+import { PageTitle, LeaderCardList, SearchInput} from '@/components/lead-trader/lead-trader-ui'
 
 const LeadersFeature = () => {
   return (
     <div className="px-4 pt-4 md:px-8">
-      <AlertSuccess msg="Team member has been added successfully." />
       <div className='pt-12 flex justify-between'>
         <PageTitle />
         <SearchInput />
